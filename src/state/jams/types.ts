6015f1ef661603fce61eec7e64fc7ea894102776
Josh Glazer <joshglazer@gmail.com>
2021-08-17 14:53:19 -0400

    feat(jams-state-management): fleshed out state management structure for jams
    added placeholder dashboard page

--- src/state/jams/types.ts
+++ src/state/jams/types.ts
@@ -1,6 +1,9 @@
-import { IJam } from "../../types";
+import { IJam, IUser } from "../../types";
 
 export interface IJamsContextData {
   myJams: IJam[] | null;
   collabJams: IJam[] | null;
+  users: {
+    [id: string]: IUser;
+  };
 }
--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Header } from "semantic-ui-react";
+import { JamList } from "../../components/jam-list";
+import { GlobalContext } from "../../state/GlobalProvider";
 
 function Dashboard() {
+  const { jamsDispatch, jamsState } = useContext(GlobalContext);
+
+  useEffect(() => {
+    jamsDispatch({ type: "loadJams" });
+  }, [jamsDispatch]);
+
   return (
     <>
       <Header as="h2">Dashboard</Header>
-      <div>Welcome welcome welcome!</div>
+      <div>Hello and Welcome to RandoJam!</div>
+
+      <JamList
+        jams={jamsState.myJams}
+        title="My Jams"
+        emptyText="You have not created any jams."
+      ></JamList>
+
+      <JamList
+        jams={jamsState.myJams}
+        title="My Collabs"
+        emptyText="You have not collabed on any jams."
+      ></JamList>
     </>
   );
 }
